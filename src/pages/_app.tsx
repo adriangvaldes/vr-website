ab3640a79b58d5d9c8c3cfb329bcd3db0baa9ed1
Adrian Valde <adriangvaldes@gmail.com>
2024-06-01 15:55:51 -0300

    Finish change to tailwind on home page & installation of shadcn/ui lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,58 @@
 import { AppProps } from "next/app";
-import Image from 'next/image';
-import vrBranco from '../assets/vrBranco.png';
-import '../global.css';
+import Image from "next/image";
+import vrBranco from "../assets/vrBranco.png";
+import "../global.css";
 import { globalStyles } from "../styles/globat";
 
-globalStyles()
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("ASDASOD");
   return (
-    <div className="flex flex-col items-start min-h-screen">
-      <header className="bg-accentDarker w-full sticky">
-        <section className="bg-accentDarker h-16 max-w-[1440px] flex justify-between items-center py-2 px-4 my-0 mx-auto">
+    <div className=''>
+      <header className='bg-accentDarker w-full sticky'>
+        <section className='bg-accentDarker h-16 max-w-[1440px] flex justify-between items-center py-2 px-4 my-0 mx-auto'>
           <Image src={vrBranco} alt='vr_branco' width={30} />
-          <menu className="flex gap-3 mr-8 box-border">
-            <a className="text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]" href="/">Home</a>
-            <a className="text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]" href="#AboutUs">Sobre nós</a>
-            <a className="text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]" href="#Products">Produtos</a>
-            <a className="text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]" href="#Tecniques">Técnicas</a>
+          <menu className='flex gap-3 mr-8 box-border'>
+            <a
+              className='text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]'
+              href='/'
+            >
+              Home
+            </a>
+            <a
+              className='text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]'
+              href='#AboutUs'
+            >
+              Sobre nós
+            </a>
+            <a
+              className='text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]'
+              href='#Products'
+            >
+              Produtos
+            </a>
+            <a
+              className='text-[0.9rem] no-underline text-textGrey transition-all duration-500 box-border hover:text-white hover:border-b hover:border-spotlight hover:mb-[-1px]'
+              href='#Tecniques'
+            >
+              Técnicas
+            </a>
           </menu>
         </section>
       </header>
-
       <Component {...pageProps} />
-      <footer className="bg-accentDarker w-full">
-        <section className="max-w-[1440px] flex min-h-[6rem] justify-between items-center p-2 mx-auto">
-          <div className="flex items-center gap-4">
+      <footer className='bg-accentDarker w-full'>
+        <section className='max-w-[1440px] flex min-h-[6rem] justify-between items-center p-2 mx-auto'>
+          <div className='flex items-center gap-4'>
             <Image src={vrBranco} alt='vr_branco' width={50} />
             <div>
-              <p className="text-textGrey">Copyright © 2022</p>
-              <p className="text-textGrey">Desenvolvido por Adrian Valdes </p>
+              <p className='text-textGrey'>Copyright © 2022</p>
+              <p className='text-textGrey'>Desenvolvido por Adrian Valdes </p>
             </div>
           </div>
         </section>
       </footer>
     </div>
-  )
+  );
 }
-
